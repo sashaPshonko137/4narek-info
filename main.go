87be7109907c55f5f7faa0f6fea7eed82a18ebe3
sashaPshonko137 <privetnitron@gmail.com>
2025-09-13 14:17:47 +0500

    lkjnnjnjjnkjknjkjknjkj

--- main.go
+++ main.go
@@ -858,7 +858,7 @@ func adjustPrice(item string) {
 		} else {
 			ratio = newRatio
 		}
-	} else if currentItemCount > sales*2 { // цена завышена
+	} else if currentItemCount + inventoryCount > sales*2 { // цена завышена
 		if sales < cfg.NormalSales { // не продаем
 			newPrice -= cfg.PriceStep
 			if newPrice < cfg.MinPrice {
